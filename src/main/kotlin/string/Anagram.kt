5b1bbe19708174af7baf1b37c8c3d9b592cb4c54
Prince <mail4prince92@gmail.com>
2023-09-04 16:32:54 +0530

    Anagram of a string

---
+++ src/main/kotlin/string/Anagram.kt
@@ -0,0 +1,29 @@
+package string
+
+/**
+ * Given two strings s and t return true if t is an anagram of s , and false otherwise.
+ * An Anagram is a word or phrase formed by rearranging the letters of a
+ * different word or phrase, typically using all the original letters exactly once.
+ */
+fun main() {
+    println("Is valid anagram Anna  & nana")
+    println(isAnagram("Anna", "nana"))
+
+    println("Is valid anagram anna  & nana")
+    println(isAnagram("anna", "nana"))
+}
+
+
+private fun isAnagram(s: String, t: String): Boolean {
+    if (s.length != t.length) return false
+
+    val characterMap = s.groupingBy { it.toLowerCase() }.eachCount().toMutableMap()
+
+    t.forEach {
+        if (!characterMap.containsKey(it)) return false
+        else {
+            characterMap[it] = characterMap[it]!! - 1
+        }
+    }
+    return characterMap.all { it.value == 0 }
+}
